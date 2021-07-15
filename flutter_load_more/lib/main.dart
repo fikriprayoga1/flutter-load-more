12e5c1fd5e4c29e83e24004134458267ce3480f3
Fikri Prayoga <fikriprayoga007@gmail.com>
2021-07-16 05:21:51 +0700

    Init done

---
+++ flutter_load_more/lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late List dummyList;
+  late ScrollController _scrollController = ScrollController();
+  int _currentMax = 10;
+
+  @override
+  void initState() {
+    super.initState();
+    dummyList = List.generate(10, (index) => 'Item : ${index + 1}');
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _getMoreList();
+      }
+    });
+  }
+
+  _getMoreList() {
+    print('Get More List');
+    for (var i = _currentMax; i < _currentMax + 10; i++) {
+      dummyList.add("Item : ${i + 1}");
+    }
+    _currentMax += 10;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lazy Loading'),
+      ),
+      body: ListView.builder(
+        controller: _scrollController,
+        itemExtent: 80,
+        itemBuilder: (context, index) {
+          if (index == dummyList.length) {
+            return CupertinoActivityIndicator();
+          }
+          return ListTile(
+            title: Text(dummyList[index]),
+          );
+        },
+        itemCount: dummyList.length + 1,
+      ),
+    );
+  }
+}
